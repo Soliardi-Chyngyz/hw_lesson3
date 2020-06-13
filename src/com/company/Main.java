cd4c5322d3a70d0a77d3f07be9e9c62c20439e49
Soliardi-Chyngyz <wxwaren@gmail.com>
2020-06-13 19:13:10 +0600

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
                 minus = true;
                 continue;
             }
-            if (counts.length > 0) {
+            if (minus) {
                 sum = (sum + task)/8;
             }
         }
